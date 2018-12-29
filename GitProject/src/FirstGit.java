08a71daff737117705bdcc020833011b285c8e99
Dell <Dell@DESKTOP-ESENG68>
2018-12-29 12:10:47 +0530

    Second Commit

--- GitProject/src/FirstGit.java
+++ GitProject/src/FirstGit.java
@@ -3,7 +3,8 @@ public class FirstGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git");
+		System.out.println("Hello Git 1");
+		System.out.println("Hello Git 2");
 	}
 
 }
